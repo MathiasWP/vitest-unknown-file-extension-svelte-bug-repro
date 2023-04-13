917a5e379877fb761f71ffc6164d41b0010ea2f1
Mathias Picker <mattis123@live.no>
2023-04-13 16:28:11 +0200

    updatred deps.inline

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,9 +7,8 @@ export default defineConfig({
     deps: {
         /**
          * One proposed solution is to add the import to deps.inline (see https://github.com/vitest-dev/vitest/issues/1395)
-         * Easy way to do this would be to set deps.inline to "true", but vitest fails if the value is true (TypeError: Cannot destructure property 'AsymmetricMatcher' of '__vite_ssr_import_1__.plugins' as it is undefined)
          */
-        inline: [] // true
+          inline: [/vitest-svelte-bug-repro\/dist/],
     }
   },
 })
